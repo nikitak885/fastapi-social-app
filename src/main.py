a7a417fc67c587589c630fa0c8fbfc68ca4a318b
Nikita Kumari <nikitak885@gmail.com>
2026-07-05 21:57:09 +0530

    Added Router

--- src/main.py
+++ src/main.py
@@ -1,34 +1,7 @@
-# main file for the code
-
-from typing import List
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-app = FastAPI()
+from src.routers.post import router as post_router
 
+app = FastAPI() 
 
-class UserPostIn(BaseModel):
-    body:str 
-    
-class UserPost(BaseModel):
-    id:int
-    
-post_table = {}
-
-@app.post("/post", response_model=UserPost)
-async def create_post(post: UserPost):
-    data = post.dict()
-    last_record_id = len(post_table)
-    new_post = {**data, "id": last_record_id}
-    post_table[last_record_id] = new_post
-    return new_post
-    
-
-    
-    
-@app.get("/post", response_model=List[UserPost])
-async def get_all_posts():
-    return list(post_table.values())
-
-
+app.include_router(post_router)
---
+++ src/routers/post.py
@@ -0,0 +1,27 @@
+# main file for the code
+
+from fastapi import APIRouter
+
+from src.models.post import UserPost, UserPostIn
+
+router = APIRouter()
+
+    
+post_table = {}
+
+@router.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.model_dump()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+    
+
+    
+    
+@router.get("/post", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
+
+
